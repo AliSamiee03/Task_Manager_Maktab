7bd454e02603acb5cb9d3a75f7f434d71b98ba50
AliSamiee <alisamiee844@gmail.com>
2024-01-27 15:56:01 +0330

    [dev][Task][models]: create base model for project

--- Tasks/admin.py
+++ Tasks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Task, Category, Tag
+
+admin.site.register([Task, Tag, Category])
 
 # Register your models here.
--- Tasks/models.py
+++ Tasks/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+class Category(models.Model):
+    name = models.CharField(max_length= 100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length= 100)
 
 class Task(models.Model):
-    Title = models.CharField(max_length = 100)
-    Description = models.TextField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    
+    title = models.CharField(max_length= 100)
+    creator = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
+    due_date = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete= models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+
     
