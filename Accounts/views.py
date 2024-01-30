8ec9f093681aa3800aea41f20e005a0964c74f18
AliSamiee <alisamiee844@gmail.com>
2024-01-30 12:10:09 +0330

    [dev][Accounts][views] : complete login and logout views

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username= username, password= password)
+            if user:
+                login(request, user)
+                return redirect('Home')
+
+
+    else:
+        form = LoginForm()
+
+    content = {'form': form}
+
+    return render(request, 'Accounts/login.html', content)
+
+def logout_view(request):
+    logout(request)
+    return redirect('Home')
