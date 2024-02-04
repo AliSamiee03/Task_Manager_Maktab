166904cf0ac53321a9dc495c1f9ca5a5dca2a15c
AliSamiee <alisamiee844@gmail.com>
2024-02-04 11:43:29 +0330

    [dev][login][template]: set messages for login

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import LoginForm
+from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(data=request.POST)
+
         if form.is_valid():
+
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(request, username= username, password= password)
-            if user:
-                login(request, user)
-                return redirect('Home')
-
-
+            request.user = authenticate(request, username= username, password= password)
+            print(f"user : {request.user}")
+            if request.user:
+                login(request, request.user)
+                messages.info(request, "success", extra_tags='success')
+                return redirect("Home")
+            else:
+                messages.error(request, "Unsuccess", extra_tags='error')
+                return redirect("login")
+            
     else:
         form = LoginForm()
 
     content = {'form': form}
-
     return render(request, 'Accounts/login.html', content)
 
 def logout_view(request):
